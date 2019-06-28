e783d762f005e65b97a208401b55581308764fd7
lemonberry99 <sheesh.mueller@gmail.com>
2019-06-28 16:38:29 +0200

    added wheel

--- KonverterSingleView/KonverterSingleView/ViewController.swift
+++ KonverterSingleView/KonverterSingleView/ViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var pickerData: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        unitPicker.delegate = self
+        unitPicker.dataSource = self
+        
+        pickerData = ["kilometer", "Euro"]
     }
-
+    
     @IBOutlet weak var inputValue: UITextField!
     @IBOutlet weak var outputValue: UILabel!
+    
+    @IBOutlet weak var unitPicker: UIPickerView!
+    
     
     @IBAction func pressedConvert(_ sender: Any) {
         
@@ -32,6 +44,20 @@ class ViewController: UIViewController {
         outputUnwrapped *= 0.621371
         outputValue.text = "\(outputUnwrapped)"
         
+    }
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 }
 
